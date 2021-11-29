4496a4d222bb0451e7bca6106a61999537bc30e1
MD Monirul Islam <mdmonirul.islam@ibm.com>
2021-11-29 00:20:31 -0500

    landing page

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     name = "Personal Budgeting"
+#     return render_template("index.html", name=name)
 
 @app.route('/')
 def home():
     name = "Personal Budgeting"
-    return render_template("index.html", name=name)
+    return render_template("home.html", name=name)
 
+@app.route('/login', methods=['POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'test' or request.form['password'] != 'test':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
